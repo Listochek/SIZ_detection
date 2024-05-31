7eb7cbbd3a7ffdeb9539a3188b653316bc905a64
Pelbmenb <artem.jidov@gmail.com>
2024-06-01 04:38:23 +0700

    mega utro 4:30

---
+++ ai_main.py
@@ -0,0 +1,6 @@
+from ultralytics import YOLO
+from multiprocessing import Pool
+import sys
+model = YOLO('yolov8m.pt')
+if __name__ == '__main__':
+    results = model.train(data="C:\SIZ_detection\data.yaml", epochs=150, imgsz=640, device=[0], model="yolov8m.pt")
