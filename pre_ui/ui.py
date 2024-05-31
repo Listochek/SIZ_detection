3e5e71aa74d3d4d0751ec9fbdc446e8b4e24ee0c
Renovatoo <kamesash251@gmail.com>
2024-05-31 18:28:35 +1100

    ыотло

--- pre_ui/ui.py
+++ pre_ui/ui.py
@@ -6,7 +6,7 @@ from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QHBoxLayout, QSlider, QPushButton, QSizePolicy, QLineEdit, QMessageBox, QTableWidget, QTableWidgetItem, QInputDialog
 import sqlite3
 
-
+#sjnfj
 class MenuWidget(QWidget):
     def __init__(self):
         super().__init__()
