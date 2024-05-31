61f41301cafa6a63af11205609baf4d60dfb5e62
Renovatoo <kamesash251@gmail.com>
2024-05-31 15:50:16 +1100

    убрал изменения

--- pre_ui/ui.py
+++ pre_ui/ui.py
@@ -6,7 +6,6 @@ from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QHBoxLayout, QSlider, QPushButton, QSizePolicy, QLineEdit, QMessageBox, QTableWidget, QTableWidgetItem, QInputDialog
 import sqlite3
 
-#проверка
 
 class MenuWidget(QWidget):
     def __init__(self):
